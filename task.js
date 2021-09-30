3174ddd4d3b75714e926e4b988446ff6fad85ae2
sajad aqeel <sajad3qeel@gmail.com>
2021-09-30 22:18:03 +0300

    js task

--- task.js
+++ task.js
@@ -1,3 +1,5 @@
+let fs = require("fs");
+
 let numbers = [
     5,
     8,
@@ -9,36 +11,52 @@ let numbers = [
     16
 ];
 
+let data = fs.readFileSync('data.txt', 'utf8').split(',');
+let output = ASC(data.map(Number)).toString();
+
+//write data To File
+function writeToFile(name,data) {
+    fs.writeFile(name, data, (err) => {
+        if (err) console.log(err);
+        console.log("Successfully Written to File.");
+    });
+}
+// asc sorting
+function ASC(numbers){
+    for(let i = 0 ; i < numbers.length ; i++)
+    {
+        for(let j = 0 ; j < numbers.length; j++)
+        {
+            if(numbers[j] > numbers[j + 1])
+            {
+                let temp = numbers[j];
+                numbers[j] = numbers[j + 1]
+                numbers[j + 1] = temp;
+            }
+        }
+    }
+    return numbers
+}
+// Desc sorting
+function DESC(numbers){
+    for(let i = 0 ; i < numbers.length ; i++)
+    {
+        for(let j = 0 ; j < numbers.length; j++)
+        {
+            if(numbers[j] < numbers[j + 1])
+            {
+                let temp = numbers[j];
+                numbers[j] = numbers[j + 1]
+                numbers[j + 1] = temp;
+            }
+        }
+    }
+    return numbers
+}
+
+// output
+writeToFile("output.txt",output)
+
 console.log("Original numbers list: ", numbers)
-
-for(let i = 0 ; i < numbers.length ; i++)
-{
-    for(let j = 0 ; j < numbers.length - 1 ; j++)
-    {
-        if(numbers[i] > numbers[j + 1])
-        {
-            let temp = numbers[j];
-            numbers[j] = numbers[j + 1]
-            numbers[j + 1] = temp;
-        }
-    }
-}
-
-console.log("Numbers list After sorting: ", numbers)
-
-for(let i = 0 ; i < numbers.length ; i++)
-{
-    for(let j = 0 ; j < numbers.length - 1 ; j++)
-    {
-        if(numbers[i] < numbers[j + 1])
-        {
-            let temp = numbers[j];
-            numbers[j] = numbers[j + 1]
-            numbers[j + 1] = temp;
-        }
-    }
-}
-
-console.log("Numbers list After Desc sorting: ", numbers)
-
-
+console.log("Numbers list After sorting: ", ASC(numbers))
+console.log("Numbers list After Desc sorting: ", DESC(numbers))
